439efba7cff496e3c686ff8a5d6eaaeb396b6f0a
Samuel Gomez <gressboldude@gmail.com>
2017-05-15 21:49:52 +0200

    added api

--- functions.js
+++ functions.js
@@ -1,15 +1,23 @@
-import private from config.js
-$(document).ready(function(){
+var API_SECRET;
+var API_CLIENT;
+$.getScript("constants.js", function(){// this function gets the values from the constants.js file
+	API_SECRET = API_SECRET;
+	API_CLIENT = API_CLIENT;
+})
 
+$(document).ready(function(){
 var data;
 $.ajax({
-	url:"https://wind-bow.gomix.me/twitch-api/channels/freecodecamp",
-	data: {
-		origin:"*"
-	},
-	headers:{"Api-user-agent": "Sam"},
+	url:"https://api.twitch.tv/kraken/games/top",
+	/*data: {
+		period:"month"
+	},*/
+	headers:{"Client-ID":API_CLIENT},
 	success: function(data) {
 		console.log(data);
+	},
+	error: function(error){
+		console.log(error)
 	}
 
 	}); //end of ajax
